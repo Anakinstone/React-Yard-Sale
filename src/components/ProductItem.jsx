8b6a967a442f0d32d4bcaa8b979e20ee527c8f68
Andres Aguilar <anakinstone@hotmail.com>
2022-01-27 19:34:24 -0600

    axios and get from api added

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 
 import btAdd from '@icons/bt_add_to_cart.svg'
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
+	const [cart, setCart] = useState([]);
+	const handleCLick = () => {
+		setCart([])
+	}
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price} USD</p>
+					<p>{product.title}</p>
 				</div>
-				<figure>
+				<figure onClick={handleCLick}>
 					<img src={btAdd} alt="" />
 				</figure>
 			</div>
